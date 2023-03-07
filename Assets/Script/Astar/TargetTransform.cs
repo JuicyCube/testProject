c4ca459fac65fd13c9672cf8ae343a5893013583
JuicyCube <117460173+JuicyCube@users.noreply.github.com>
2023-03-07 11:58:04 +0800

    astar with transformarray not working as intended

---
+++ Assets/Script/Astar/TargetTransform.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class TargetTransform : MonoBehaviour
+{
+    public Vector3[] targetPositions;
+}
--- Assets/Script/ObjectInstantiator.cs
+++ Assets/Script/ObjectInstantiator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class ObjectInstantiator : MonoBehaviour
 {
@@ -12,21 +13,31 @@ public class ObjectInstantiator : MonoBehaviour
     private GameObject prefab1Instance;
     private GameObject prefab2Instance;
     private int count = 10;
+    [SerializeField] Transform[] prefab1Transforms;
+    private AIDestinationSetter destinationSetter;
 
     private void Start()
     {
+        prefab1Transforms = new Transform[count];
+
         for (int i = 0; i < count; i++)
         {
             if (i % 2 == 0)
             {
                 prefab1Instance = InstantiateOnGround(prefab1);
-                
+                prefab1Transforms[i] = prefab1Instance.transform;
             }
             else
             {
                 prefab2Instance = InstantiateOnGround(prefab2);
             }
         }
+
+        // Get the AIDestinationSetter component attached to the game object that this script is attached to
+        destinationSetter = GetComponent<AIDestinationSetter>();
+
+        // Set the target transform of the AIDestinationSetter component to the first element in the prefab1Transforms array
+        destinationSetter.target = prefab1Transforms[0];
     }
 
     private GameObject InstantiateOnGround(GameObject prefab)
