91aaaa0ffec40a5425a9daa5492cf9c786d35bb8
JuicyCube <117460173+JuicyCube@users.noreply.github.com>
2023-03-08 15:09:03 +0800

    enemyAI attack and walking function

--- Assets/Script/Enemy/EnemyAI.cs
+++ Assets/Script/Enemy/EnemyAI.cs
@@ -7,8 +7,11 @@ public class EnemyAI : MonoBehaviour
     public float movementSpeed = 1f;
     public float rotationSpeed = 4f;
     public Animator animator;
+    public float attackRange = 2f;
+    public float attackDelay = 1f;
 
     private Transform player;
+    private bool canAttack = true;
 
     void Start()
     {
@@ -16,17 +19,41 @@ public class EnemyAI : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-
     void Update()
     {
         // Rotate towards the player
-        // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), rotationSpeed * Time.deltaTime);
 
         // Move towards the player
-        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer > attackRange)
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            if (canAttack)
+            {
+                StartCoroutine(Attack());
+            }
+        }
 
         // Set animation speed based on movement speed
         animator.SetFloat("Speed", movementSpeed);
     }
+
+    IEnumerator Attack()
+    {
+        canAttack = false;
+
+        // Play attack animation
+        animator.SetTrigger("Attack");
+
+        // Wait for attack delay
+        yield return new WaitForSeconds(attackDelay);
+
+        canAttack = true;
+    }
 }
+
 
